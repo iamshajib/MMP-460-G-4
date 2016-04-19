f9a1fea860685013aefd6d9e1645823f8bf52a21
Mohammed Shajib <mohammed.shajib@f1006-01-34125.bmcc.cuny.edu>
2016-04-19 19:35:39 -0400

    adding the theme

---
+++ wordpress/wp-content/themes/group-4/footer.php
@@ -0,0 +1,15 @@
+<footer class="footer">
+        <div class="container">
+            <p class="text-muted">&copy; 2015 Christopher Stein
+        </div>
+    </footer>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <?php wp_footer(); // js scripts are inserted using this function ?>
+</body>
+
+</html>
