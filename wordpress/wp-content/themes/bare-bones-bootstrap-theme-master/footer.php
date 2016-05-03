bc522d77392a31020c6beae7f98b50b62197e006
Mohammed Shajib <mohammed.shajib@f1006-01-34125.bmcc.cuny.edu>
2016-05-03 18:01:24 -0400

    add theme

---
+++ wordpress/wp-content/themes/bare-bones-bootstrap-theme-master/footer.php
@@ -0,0 +1,10 @@
+    <footer class="footer">
+        <div class="container">
+            <p class="text-muted">&copy; <?php the_date('Y'); ?> <?php bloginfo('name'); ?>
+        </div>
+    </footer>
+
+    <?php wp_footer(); // js scripts are inserted using this function ?>
+</body>
+
+</html>
