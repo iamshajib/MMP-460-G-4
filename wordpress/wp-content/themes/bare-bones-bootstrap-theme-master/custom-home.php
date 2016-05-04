e91e2647332daa1a7aad9792c170b865b9513def
Mohammed Shajib <mohammed.shajib@f1006-01-34125.bmcc.cuny.edu>
2016-05-03 22:01:51 -0400

    done

---
+++ wordpress/wp-content/themes/bare-bones-bootstrap-theme-master/custom-home.php
@@ -0,0 +1,13 @@
+<?php /* Template Name: Custom Home Page Template */ ?>
+    <?php get_header(); ?>
+
+
+        <div class="wrapper">
+
+
+<h1> Home Shajib</h1>
+
+
+</div>   <!-- end of wrapper -->
+
+        <?php get_footer(); ?>
---
+++ wordpress/wp-content/themes/bare-bones-bootstrap-theme-master/custom-vat.php
@@ -0,0 +1,250 @@
+<?php /* Template Name: Custom Vat Page Template */ ?>
+    <?php get_header(); ?>
+
+
+
+
+
+
+
+
+<div class="container-fluid">
+
+
+                <div id="myCarousel" class="carousel slide" data-interval="6500" data-ride="carousel">
+                    <!-- Carousel indicators -->
+                    <ol class="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                    </ol>
+                    <!-- Carousel items -->
+                    <div class="carousel-inner">
+                        <div class="active item carousel-fade">
+                            <h2>Slide 1</h2>
+                            <div class="carousel-caption">
+                                <h3>First slide label</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                        </div>
+                        <div class="item carousel-fade">
+                            <h2>Slide 2</h2>
+                            <div class="carousel-caption">
+                                <h3>Second slide label</h3>
+                                <p>Aliquam sit amet gravida nibh, facilisis gravida odio.</p>
+                            </div>
+                        </div>
+                        <div class="item carousel-fade">
+                            <h2>Slide 3</h2>
+                            <div class="carousel-caption">
+                                <h3>Third slide label</h3>
+                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Carousel nav -->
+                    <a class="carousel-control left" href="#myCarousel" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left"></span>
+                    </a>
+                    <a class="carousel-control right" href="#myCarousel" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right"></span>
+                    </a>
+                </div>
+ </div>
+
+
+
+<div class="container" id="vat">
+
+
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    Welcome to Video Arts & Technology
+                </h1>
+            </div>
+
+            <div class="col-md-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3>About</h3>
+                    </div>
+                    <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
+                        <a href="#" class="btn btn-default">Learn More</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3>Courses</h3>
+                    </div>
+                    <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
+                        <a href="#" class="btn btn-default">Learn More</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+
+            <!-- Team Members -->
+            <div class="row">
+
+                <div class="col-lg-12">
+                    <h1 class="page-header">Students</h1>
+                </div>
+
+                <div class="col-md-12">
+
+
+
+                        <div class="col-md-3 text-center">
+                            <div class="thumbnail">
+                                <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                                <div class="caption">
+                                    <h3>John Smith<br>
+                            <small>Area of Study</small>
+                        </h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 text-center">
+                            <div class="thumbnail">
+                                <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                                <div class="caption">
+                                    <h3>John Smith<br>
+                            <small>Area of Study</small>
+                        </h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 text-center">
+                            <div class="thumbnail">
+                                <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                                <div class="caption">
+                                    <h3>John Smith<br>
+                            <small>Area of Study</small>
+                        </h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 text-center">
+                            <div class="thumbnail">
+                                <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                                <div class="caption">
+                                    <h3>John Smith<br>
+                            <small>Area of Study</small>
+                        </h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 text-center">
+                            <div class="thumbnail">
+                                <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                                <div class="caption">
+                                    <h3>John Smith<br>
+                            <small>Area of Study</small>
+                        </h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 text-center">
+                            <div class="thumbnail">
+                                <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                                <div class="caption">
+                                    <h3>John Smith<br>
+                            <small>Area of Study</small>
+                        </h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 text-center">
+                            <div class="thumbnail">
+                                <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                                <div class="caption">
+                                    <h3>John Smith<br>
+                            <small>Area of Study</small>
+                        </h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 text-center">
+                            <div class="thumbnail">
+                                <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                                <div class="caption">
+                                    <h3>John Smith<br>
+                            <small>Area of Study</small>
+                        </h3>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    <!-- end of students list-->
+
+
+                </div>
+
+            </div>
+            <!-- end of Team Members -->
+
+            <hr>
+
+            <!-- Pagination -->
+            <div class="row text-center">
+                <div class="col-lg-12">
+                    <ul class="pagination">
+                        <li>
+                            <a href="#">&laquo;</a>
+                        </li>
+                        <li class="active">
+                            <a href="#">1</a>
+                        </li>
+                        <li>
+                            <a href="#">2</a>
+                        </li>
+                        <li>
+                            <a href="#">3</a>
+                        </li>
+                        <li>
+                            <a href="#">4</a>
+                        </li>
+                        <li>
+                            <a href="#">5</a>
+                        </li>
+                        <li>
+                            <a href="#">&raquo;</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- /.row -->
+
+
+            <hr>
+
+
+
+            <!-- Footer -->
+            <footer>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <p>Copyright &copy; Your Website 2014</p>
+                    </div>
+                </div>
+            </footer>
+
+        </div>
+        <!--- end of container--->
+
+
+
+
+
+
+        <?php get_footer(); ?>
